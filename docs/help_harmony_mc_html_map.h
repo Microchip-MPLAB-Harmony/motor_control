f174d8c798714b531c3db7a97079b2046e15d759
Prachi Phadke <prachi.phadke@microchip.com>
2019-03-08 16:50:31 +0530

    [doc] Updated master document
     - Added document for acim_vhz_sam_c21
     - Updated document pmsm_foc_encoder_sam_e70
    
    MH3-7675 MH3-7676

--- docs/help_harmony_mc_html_map.h
+++ docs/help_harmony_mc_html_map.h
@@ -1,31 +1,38 @@
 #define IDH_HTML_Motor_Control_Overview                    0x00000000 // Motor Control Overview
 #define IDH_HTML_X2CScope_Plugin                           0x00000001 // X2CScope Plugin
 #define IDH_HTML_Applications_Help                         0x00000002 // Applications Help
-#define IDH_HTML_pmsm_foc_Applications_Help                0x00000003 // pmsm_foc Applications Help
-#define IDH_HTML_pmsm_foc_Applications_pmsm_foc_pll_estimator_sam_e70 0x00000004 // pmsm_foc Applications pmsm_foc_pll_estimator_sam_e70
-#define IDH_HTML_pmsm_foc_pmsm_foc_pll_estimator_sam_e70_Building_The_Application 0x00000005 // pmsm_foc pmsm_foc_pll_estimator_sam_e70 Building The Application
-#define IDH_HTML_pmsm_foc_pmsm_foc_pll_estimator_sam_e70_MPLAB_Harmony_Configurations 0x00000006 // pmsm_foc pmsm_foc_pll_estimator_sam_e70 MPLAB Harmony Configurations
-#define IDH_HTML_pmsm_foc_pmsm_foc_pll_estimator_sam_e70_Control_Algorithm 0x00000007 // pmsm_foc pmsm_foc_pll_estimator_sam_e70 Control Algorithm
-#define IDH_HTML_pmsm_foc_pmsm_foc_pll_estimator_sam_e70_Hardware_Setup 0x00000008 // pmsm_foc pmsm_foc_pll_estimator_sam_e70 Hardware Setup
-#define IDH_HTML_pmsm_foc_pmsm_foc_pll_estimator_sam_e70_Running_The_Application 0x00000009 // pmsm_foc pmsm_foc_pll_estimator_sam_e70 Running The Application
-#define IDH_HTML_pmsm_foc_encoder_Applications_Help        0x0000000A // pmsm_foc_encoder Applications Help
-#define IDH_HTML_pmsm_foc_encoder_Applications_pmsm_foc_encoder_sam_e70 0x0000000B // pmsm_foc_encoder Applications pmsm_foc_encoder_sam_e70
-#define IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Building_The_Application 0x0000000C // pmsm_foc_encoder pmsm_foc_encoder_sam_e70 Building The Application
-#define IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_MPLAB_Harmony_Configurations 0x0000000D // pmsm_foc_encoder pmsm_foc_encoder_sam_e70 MPLAB Harmony Configurations
-#define IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Control_Algorithm 0x0000000E // pmsm_foc_encoder pmsm_foc_encoder_sam_e70 Control Algorithm
-#define IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Hardware_Setup 0x0000000F // pmsm_foc_encoder pmsm_foc_encoder_sam_e70 Hardware Setup
-#define IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Running_The_Application 0x00000010 // pmsm_foc_encoder pmsm_foc_encoder_sam_e70 Running The Application
-#define IDH_HTML_pmsm_foc_rolo_Applications_Help           0x00000011 // pmsm_foc_rolo Applications Help
-#define IDH_HTML_pmsm_foc_rolo_Applications_pmsm_foc_rolo_sam_c21 0x00000012 // pmsm_foc_rolo Applications pmsm_foc_rolo_sam_c21
-#define IDH_HTML_pmsm_foc_rolo_pmsm_foc_rolo_sam_c21_Building_The_Application 0x00000013 // pmsm_foc_rolo pmsm_foc_rolo_sam_c21 Building The Application
-#define IDH_HTML_pmsm_foc_rolo_pmsm_foc_rolo_sam_c21_MPLAB_Harmony_Configurations 0x00000014 // pmsm_foc_rolo pmsm_foc_rolo_sam_c21 MPLAB Harmony Configurations
-#define IDH_HTML_pmsm_foc_rolo_pmsm_foc_rolo_sam_c21_Control_Algorithm 0x00000015 // pmsm_foc_rolo pmsm_foc_rolo_sam_c21 Control Algorithm
-#define IDH_HTML_pmsm_foc_rolo_pmsm_foc_rolo_sam_c21_Hardware_Setup 0x00000016 // pmsm_foc_rolo pmsm_foc_rolo_sam_c21 Hardware Setup
-#define IDH_HTML_pmsm_foc_rolo_pmsm_foc_rolo_sam_c21_Running_The_Application 0x00000017 // pmsm_foc_rolo pmsm_foc_rolo_sam_c21 Running The Application
-#define IDH_HTML_pmsm_foc_rolo_wm_Applications_Help        0x00000018 // pmsm_foc_rolo_wm Applications Help
-#define IDH_HTML_pmsm_foc_rolo_wm_Applications_pmsm_foc_rolo_wm_sam_c21 0x00000019 // pmsm_foc_rolo_wm Applications pmsm_foc_rolo_wm_sam_c21
-#define IDH_HTML_pmsm_foc_rolo_wm_pmsm_foc_rolo_wm_sam_c21_Building_The_Application 0x0000001A // pmsm_foc_rolo_wm pmsm_foc_rolo_wm_sam_c21 Building The Application
-#define IDH_HTML_pmsm_foc_rolo_wm_pmsm_foc_rolo_wm_sam_c21_MPLAB_Harmony_Configurations 0x0000001B // pmsm_foc_rolo_wm pmsm_foc_rolo_wm_sam_c21 MPLAB Harmony Configurations
-#define IDH_HTML_pmsm_foc_rolo_wm_pmsm_foc_rolo_wm_sam_c21_Control_Algorithm 0x0000001C // pmsm_foc_rolo_wm pmsm_foc_rolo_wm_sam_c21 Control Algorithm
-#define IDH_HTML_pmsm_foc_rolo_wm_pmsm_foc_rolo_wm_sam_c21_Hardware_Setup 0x0000001D // pmsm_foc_rolo_wm pmsm_foc_rolo_wm_sam_c21 Hardware Setup
-#define IDH_HTML_pmsm_foc_rolo_wm_pmsm_foc_rolo_wm_sam_c21_Running_The_Application 0x0000001E // pmsm_foc_rolo_wm pmsm_foc_rolo_wm_sam_c21 Running The Application
+#define IDH_HTML_acim_vhz_Applications_Help                0x00000003 // acim_vhz Applications Help
+#define IDH_HTML_acim_vhz_Applications_acim_vhz_sam_c21    0x00000004 // acim_vhz Applications acim_vhz_sam_c21
+#define IDH_HTML_acim_vhz_acim_vhz_sam_c21_Building_The_Application 0x00000005 // acim_vhz acim_vhz_sam_c21 Building The Application
+#define IDH_HTML_acim_vhz_acim_vhz_sam_c21_MPLAB_Harmony_Configurations 0x00000006 // acim_vhz acim_vhz_sam_c21 MPLAB Harmony Configurations
+#define IDH_HTML_acim_vhz_acim_vhz_sam_c21_Control_Algorithm 0x00000007 // acim_vhz acim_vhz_sam_c21 Control Algorithm
+#define IDH_HTML_acim_vhz_acim_vhz_sam_c21_Hardware_Setup  0x00000008 // acim_vhz acim_vhz_sam_c21 Hardware Setup
+#define IDH_HTML_acim_vhz_acim_vhz_sam_c21_Running_The_Application 0x00000009 // acim_vhz acim_vhz_sam_c21 Running The Application
+#define IDH_HTML_pmsm_foc_Applications_Help                0x0000000A // pmsm_foc Applications Help
+#define IDH_HTML_pmsm_foc_Applications_pmsm_foc_pll_estimator_sam_e70 0x0000000B // pmsm_foc Applications pmsm_foc_pll_estimator_sam_e70
+#define IDH_HTML_pmsm_foc_pmsm_foc_pll_estimator_sam_e70_Building_The_Application 0x0000000C // pmsm_foc pmsm_foc_pll_estimator_sam_e70 Building The Application
+#define IDH_HTML_pmsm_foc_pmsm_foc_pll_estimator_sam_e70_MPLAB_Harmony_Configurations 0x0000000D // pmsm_foc pmsm_foc_pll_estimator_sam_e70 MPLAB Harmony Configurations
+#define IDH_HTML_pmsm_foc_pmsm_foc_pll_estimator_sam_e70_Control_Algorithm 0x0000000E // pmsm_foc pmsm_foc_pll_estimator_sam_e70 Control Algorithm
+#define IDH_HTML_pmsm_foc_pmsm_foc_pll_estimator_sam_e70_Hardware_Setup 0x0000000F // pmsm_foc pmsm_foc_pll_estimator_sam_e70 Hardware Setup
+#define IDH_HTML_pmsm_foc_pmsm_foc_pll_estimator_sam_e70_Running_The_Application 0x00000010 // pmsm_foc pmsm_foc_pll_estimator_sam_e70 Running The Application
+#define IDH_HTML_pmsm_foc_encoder_Applications_Help        0x00000011 // pmsm_foc_encoder Applications Help
+#define IDH_HTML_pmsm_foc_encoder_Applications_pmsm_foc_encoder_sam_e70 0x00000012 // pmsm_foc_encoder Applications pmsm_foc_encoder_sam_e70
+#define IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Building_The_Application 0x00000013 // pmsm_foc_encoder pmsm_foc_encoder_sam_e70 Building The Application
+#define IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_MPLAB_Harmony_Configurations 0x00000014 // pmsm_foc_encoder pmsm_foc_encoder_sam_e70 MPLAB Harmony Configurations
+#define IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_control_algorithm 0x00000015 // pmsm_foc_encoder pmsm_foc_encoder_sam_e70_control_algorithm
+#define IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Hardware_Setup 0x00000016 // pmsm_foc_encoder pmsm_foc_encoder_sam_e70 Hardware Setup
+#define IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Running_The_Application 0x00000017 // pmsm_foc_encoder pmsm_foc_encoder_sam_e70 Running The Application
+#define IDH_HTML_pmsm_foc_rolo_Applications_Help           0x00000018 // pmsm_foc_rolo Applications Help
+#define IDH_HTML_pmsm_foc_rolo_Applications_pmsm_foc_rolo_sam_c21 0x00000019 // pmsm_foc_rolo Applications pmsm_foc_rolo_sam_c21
+#define IDH_HTML_pmsm_foc_rolo_pmsm_foc_rolo_sam_c21_Building_The_Application 0x0000001A // pmsm_foc_rolo pmsm_foc_rolo_sam_c21 Building The Application
+#define IDH_HTML_pmsm_foc_rolo_pmsm_foc_rolo_sam_c21_MPLAB_Harmony_Configurations 0x0000001B // pmsm_foc_rolo pmsm_foc_rolo_sam_c21 MPLAB Harmony Configurations
+#define IDH_HTML_pmsm_foc_rolo_pmsm_foc_rolo_sam_c21_Control_Algorithm 0x0000001C // pmsm_foc_rolo pmsm_foc_rolo_sam_c21 Control Algorithm
+#define IDH_HTML_pmsm_foc_rolo_pmsm_foc_rolo_sam_c21_Hardware_Setup 0x0000001D // pmsm_foc_rolo pmsm_foc_rolo_sam_c21 Hardware Setup
+#define IDH_HTML_pmsm_foc_rolo_pmsm_foc_rolo_sam_c21_Running_The_Application 0x0000001E // pmsm_foc_rolo pmsm_foc_rolo_sam_c21 Running The Application
+#define IDH_HTML_pmsm_foc_rolo_wm_Applications_Help        0x0000001F // pmsm_foc_rolo_wm Applications Help
+#define IDH_HTML_pmsm_foc_rolo_wm_Applications_pmsm_foc_rolo_wm_sam_c21 0x00000020 // pmsm_foc_rolo_wm Applications pmsm_foc_rolo_wm_sam_c21
+#define IDH_HTML_pmsm_foc_rolo_wm_pmsm_foc_rolo_wm_sam_c21_Building_The_Application 0x00000021 // pmsm_foc_rolo_wm pmsm_foc_rolo_wm_sam_c21 Building The Application
+#define IDH_HTML_pmsm_foc_rolo_wm_pmsm_foc_rolo_wm_sam_c21_MPLAB_Harmony_Configurations 0x00000022 // pmsm_foc_rolo_wm pmsm_foc_rolo_wm_sam_c21 MPLAB Harmony Configurations
+#define IDH_HTML_pmsm_foc_rolo_wm_pmsm_foc_rolo_wm_sam_c21_Control_Algorithm 0x00000023 // pmsm_foc_rolo_wm pmsm_foc_rolo_wm_sam_c21 Control Algorithm
+#define IDH_HTML_pmsm_foc_rolo_wm_pmsm_foc_rolo_wm_sam_c21_Hardware_Setup 0x00000024 // pmsm_foc_rolo_wm pmsm_foc_rolo_wm_sam_c21 Hardware Setup
+#define IDH_HTML_pmsm_foc_rolo_wm_pmsm_foc_rolo_wm_sam_c21_Running_The_Application 0x00000025 // pmsm_foc_rolo_wm pmsm_foc_rolo_wm_sam_c21 Running The Application
--- docs/help_harmony_mc_html_mapfunc.c
+++ docs/help_harmony_mc_html_mapfunc.c
@@ -8,6 +8,13 @@ int HTML_ID_Mapping(int id)
     case IDH_Motor_Control_Overview                            : return IDH_HTML_Motor_Control_Overview;
     case IDH_X2CScope_Plugin                                   : return IDH_HTML_X2CScope_Plugin;
     case IDH_Applications_Help                                 : return IDH_HTML_Applications_Help;
+    case IDH_acim_vhz_Applications_Help                        : return IDH_HTML_acim_vhz_Applications_Help;
+    case IDH_acim_vhz_Applications_acim_vhz_sam_c21            : return IDH_HTML_acim_vhz_Applications_acim_vhz_sam_c21;
+    case IDH_acim_vhz_acim_vhz_sam_c21_Building_The_Application: return IDH_HTML_acim_vhz_acim_vhz_sam_c21_Building_The_Application;
+    case IDH_acim_vhz_acim_vhz_sam_c21_MPLAB_Harmony_Configurations: return IDH_HTML_acim_vhz_acim_vhz_sam_c21_MPLAB_Harmony_Configurations;
+    case IDH_acim_vhz_acim_vhz_sam_c21_Control_Algorithm       : return IDH_HTML_acim_vhz_acim_vhz_sam_c21_Control_Algorithm;
+    case IDH_acim_vhz_acim_vhz_sam_c21_Hardware_Setup          : return IDH_HTML_acim_vhz_acim_vhz_sam_c21_Hardware_Setup;
+    case IDH_acim_vhz_acim_vhz_sam_c21_Running_The_Application : return IDH_HTML_acim_vhz_acim_vhz_sam_c21_Running_The_Application;
     case IDH_pmsm_foc_Applications_Help                        : return IDH_HTML_pmsm_foc_Applications_Help;
     case IDH_pmsm_foc_Applications_pmsm_foc_pll_estimator_sam_e70: return IDH_HTML_pmsm_foc_Applications_pmsm_foc_pll_estimator_sam_e70;
     case IDH_pmsm_foc_pmsm_foc_pll_estimator_sam_e70_Building_The_Application: return IDH_HTML_pmsm_foc_pmsm_foc_pll_estimator_sam_e70_Building_The_Application;
@@ -19,7 +26,7 @@ int HTML_ID_Mapping(int id)
     case IDH_pmsm_foc_encoder_Applications_pmsm_foc_encoder_sam_e70: return IDH_HTML_pmsm_foc_encoder_Applications_pmsm_foc_encoder_sam_e70;
     case IDH_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Building_The_Application: return IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Building_The_Application;
     case IDH_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_MPLAB_Harmony_Configurations: return IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_MPLAB_Harmony_Configurations;
-    case IDH_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Control_Algorithm: return IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Control_Algorithm;
+    case IDH_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_control_algorithm: return IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_control_algorithm;
     case IDH_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Hardware_Setup: return IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Hardware_Setup;
     case IDH_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Running_The_Application: return IDH_HTML_pmsm_foc_encoder_pmsm_foc_encoder_sam_e70_Running_The_Application;
     case IDH_pmsm_foc_rolo_Applications_Help                   : return IDH_HTML_pmsm_foc_rolo_Applications_Help;
