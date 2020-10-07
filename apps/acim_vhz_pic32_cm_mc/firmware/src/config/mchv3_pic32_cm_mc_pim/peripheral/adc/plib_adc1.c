4954b730dc111f483f7ba54ba5a8ec7b7dc22c7d
Utsav Somani <Utsav.Somani@microchip.com>
2020-10-07 13:57:05 -0700

    [ACIM_VHz] Regenerated acim_vhz_pic32_cm_mc MCHV3 MH3-44592

---
+++ apps/acim_vhz_pic32_cm_mc/firmware/src/config/mchv3_pic32_cm_mc_pim/peripheral/adc/plib_adc1.c
@@ -0,0 +1,222 @@
+/*******************************************************************************
+  Analog-to-Digital Converter(ADC1) PLIB
+
+  Company
+    Microchip Technology Inc.
+
+  File Name
+    plib_adc1.c
+
+  Summary
+    ADC1 PLIB Implementation File.
+
+  Description
+    This file defines the interface to the ADC peripheral library. This
+    library provides access to and control of the associated peripheral
+    instance.
+
+  Remarks:
+    None.
+
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+/* This section lists the other files that are included in this file.
+*/
+
+#include "plib_adc1.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data
+// *****************************************************************************
+// *****************************************************************************
+ADC_CALLBACK_OBJ ADC1_CallbackObject;
+
+#define ADC1_LINEARITY_POS  (6)
+#define ADC1_LINEARITY_Msk   (0x7 << ADC1_LINEARITY_POS)
+
+#define ADC1_BIASCAL_POS  (9)
+#define ADC1_BIASCAL_Msk   (0x7 << ADC1_BIASCAL_POS)
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: ADC1 Implementation
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Initialize ADC module */
+void ADC1_Initialize( void )
+{
+    /* Reset ADC */
+    ADC1_REGS->ADC_CTRLA = ADC_CTRLA_SWRST_Msk;
+
+    while((ADC1_REGS->ADC_SYNCBUSY & ADC_SYNCBUSY_SWRST_Msk) == ADC_SYNCBUSY_SWRST_Msk)
+    {
+        /* Wait for Synchronization */
+    }
+    /* Write linearity calibration in BIASREFBUF and bias calibration in BIASCOMP */
+    ADC1_REGS->ADC_CALIB = (uint32_t)(ADC_CALIB_BIASREFBUF(((*(uint64_t*)OTP5_ADDR) & ADC1_LINEARITY_Msk))) \
+        | ADC_CALIB_BIASCOMP((((*(uint64_t*)OTP5_ADDR) & ADC1_BIASCAL_Msk) >> ADC1_BIASCAL_POS));
+
+    /* Prescaler */
+    ADC1_REGS->ADC_CTRLB = ADC_CTRLB_PRESCALER_DIV8;
+    /* Sampling length */
+    ADC1_REGS->ADC_SAMPCTRL = ADC_SAMPCTRL_SAMPLEN(3U);
+
+    /* Reference */
+    ADC1_REGS->ADC_REFCTRL = ADC_REFCTRL_REFSEL_INTVCC2;
+
+    /* Input pin */
+    ADC1_REGS->ADC_INPUTCTRL = (uint16_t) ADC_POSINPUT_AIN2;
+
+    /* Resolution & Operation Mode */
+    ADC1_REGS->ADC_CTRLC = ADC_CTRLC_RESSEL_12BIT | ADC_CTRLC_WINMODE(0) ;
+
+
+    /* Clear all interrupt flags */
+    ADC1_REGS->ADC_INTFLAG = ADC_INTFLAG_Msk;
+    /* Enable interrupts */
+    ADC1_REGS->ADC_INTENSET = ADC_INTENSET_RESRDY_Msk;
+    /* Events configuration  */
+    ADC1_REGS->ADC_EVCTRL = ADC_EVCTRL_STARTEI_Msk;
+
+    while(ADC1_REGS->ADC_SYNCBUSY)
+    {
+        /* Wait for Synchronization */
+    }
+}
+
+/* Enable ADC module */
+void ADC1_Enable( void )
+{
+    ADC1_REGS->ADC_CTRLA |= ADC_CTRLA_ENABLE_Msk;
+    while(ADC1_REGS->ADC_SYNCBUSY)
+    {
+        /* Wait for Synchronization */
+    }
+}
+
+/* Disable ADC module */
+void ADC1_Disable( void )
+{
+    ADC1_REGS->ADC_CTRLA &= ~ADC_CTRLA_ENABLE_Msk;
+    while(ADC1_REGS->ADC_SYNCBUSY)
+    {
+        /* Wait for Synchronization */
+    }
+}
+
+/* Configure channel input */
+void ADC1_ChannelSelect( ADC_POSINPUT positiveInput, ADC_NEGINPUT negativeInput )
+{
+    /* Configure pin scan mode and positive and negative input pins */
+    ADC1_REGS->ADC_INPUTCTRL = (uint16_t) positiveInput | (uint16_t) negativeInput;
+
+    while((ADC1_REGS->ADC_SYNCBUSY & ADC_SYNCBUSY_INPUTCTRL_Msk) == ADC_SYNCBUSY_INPUTCTRL_Msk)
+    {
+        /* Wait for Synchronization */
+    }
+}
+
+/* Start the ADC conversion by SW */
+void ADC1_ConversionStart( void )
+{
+    /* Start conversion */
+    ADC1_REGS->ADC_SWTRIG |= ADC_SWTRIG_START_Msk;
+
+    while((ADC1_REGS->ADC_SYNCBUSY & ADC_SYNCBUSY_SWTRIG_Msk) == ADC_SYNCBUSY_SWTRIG_Msk)
+    {
+        /* Wait for Synchronization */
+    }
+}
+
+/* Check whether auto sequence conversion is done */
+bool ADC1_ConversionSequenceIsFinished(void)
+{
+    bool seq_status = false;
+    if ((ADC1_REGS->ADC_SEQSTATUS & ADC_SEQSTATUS_SEQBUSY_Msk) != ADC_SEQSTATUS_SEQBUSY_Msk)
+    {
+        seq_status = true;
+    }
+    return seq_status;
+}
+
+/* Configure window comparison threshold values */
+void ADC1_ComparisonWindowSet(uint16_t low_threshold, uint16_t high_threshold)
+{
+    ADC1_REGS->ADC_WINLT = low_threshold;
+    ADC1_REGS->ADC_WINUT = high_threshold;
+    while((ADC1_REGS->ADC_SYNCBUSY))
+    {
+        /* Wait for Synchronization */
+    }
+}
+
+void ADC1_WindowModeSet(ADC_WINMODE mode)
+{
+    ADC1_REGS->ADC_CTRLC &= ~ADC_CTRLC_WINMODE_Msk;
+    ADC1_REGS->ADC_CTRLC |= (mode << ADC_CTRLC_WINMODE_Pos);
+    while((ADC1_REGS->ADC_SYNCBUSY))
+    {
+        /* Wait for Synchronization */
+    }
+}
+
+/* Read the conversion result */
+uint16_t ADC1_ConversionResultGet( void )
+{
+    return (uint16_t)ADC1_REGS->ADC_RESULT;
+}
+
+/* Register callback function */
+void ADC1_CallbackRegister( ADC_CALLBACK callback, uintptr_t context )
+{
+    ADC1_CallbackObject.callback = callback;
+
+    ADC1_CallbackObject.context = context;
+}
+
+
+void ADC1_InterruptHandler( void )
+{
+    volatile ADC_STATUS status;
+    status = ADC1_REGS->ADC_INTFLAG;
+    /* Clear interrupt flag */
+    ADC1_REGS->ADC_INTFLAG = ADC_INTENSET_RESRDY_Msk;
+    if (ADC1_CallbackObject.callback != NULL)
+    {
+        ADC1_CallbackObject.callback(status, ADC1_CallbackObject.context);
+    }
+}
---
+++ apps/acim_vhz_pic32_cm_mc/firmware/src/config/mchv3_pic32_cm_mc_pim/userparams.h
@@ -0,0 +1,94 @@
+/*******************************************************************************
+  System Definitions
+
+  File Name:
+    userparams.h
+
+  Summary:
+    Header file which defines Motor Specific and Board Specific constants 
+
+  Description:
+    This file contains the motor and board specific constants. It also defines
+ * switches which allows algorithm to be run in debug modes like Open Loop Mode,
+ * Torque mode, etc. 
+
+ *******************************************************************************/
+
+//DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+//DOM-IGNORE-END
+
+#ifndef USERPARAMS_H 
+#define USERPARAMS_H
+
+/*Define the Motor Type*/
+#define AC_IM_1    
+
+/*******************************************************************************
+Macro definitions
+*******************************************************************************/
+#define MC_FREQ_HZ      ( 48000000.0f ) /* PWM peripheral frequency */
+
+#define PWM_FREQUENCY 20       /* in kHz   */
+
+#define PWM_HPER_TICKS       ( 1200U )  /* 4000 ticks, total period 8000 ticks @48MHz -> 167us */
+#define HALF_PWM_HPER_TICKS  ( PWM_HPER_TICKS >> 1)
+/* 1200 ticks , total period 2400 ticks -> 50 micro seconds */
+
+/* motor and application related parameters */
+/* Note: only one motor has to be selected! */
+
+#ifdef  AC_IM_1                     /* ACIM Example motor */
+
+#define MOTOR_VOLTAGE           (230)                   /* Motor Voltage [Volts] */
+#define MAX_MOTOR_SPEED         (1800)                  /* maximum motor speed [RPM] */
+#define NUMBER_OF_POLES         (4)                     /* number of poles  */
+#define MAX_SPEED_SCALED        (16384U)                /* Max speed in internal Units */
+#define VF_CONSTANT             ((PWM_HPER_TICKS << 12) /MAX_SPEED_SCALED)      /* (PWM_HPER_TICKS/MAX_SPEED_SCALED) * 2^12 */
+#define VF_OFFSET               (0.09f * PWM_HPER_TICKS)/* Voltage offset at starting zero freq; Approx (0.09 * PWM_HPER_TICKS)  */
+#define START_SPEED_DEFAULT     (200U)                  /* Default startup speed */
+#define ACC_RAMP                (1)                     /* acceleration ramp count in internal unit */
+#define DEC_RAMP                (1)                     /* deceleration ramp count in internal unit */
+#define SPEED_FILTER_COEFF      (10)                    /* Speed filter coefficient range [1-16]    */
+
+#endif  /* ifdef AC_IM_1 */
+
+#ifdef  AC_IM_2                     /* ACIM Example motor */
+
+#define MOTOR_VOLTAGE           (230)                   /* Motor Voltage [Volts] */
+#define MAX_MOTOR_SPEED         (3600)                  /* maximum motor speed [RPM] */
+#define NUMBER_OF_POLES         (2)                     /* number of poles  */
+#define MAX_SPEED_SCALED        (16384U)                /* Max speed in internal Units */
+#define VF_CONSTANT             ((PWM_HPER_TICKS << 12)/MAX_SPEED_SCALED)       /* (PWM_HPER_TICKS/MAX_SPEED_SCALED) * 2^12 */
+#define VF_OFFSET               (0.09f * PWM_HPER_TICKS)/* Voltage offset at starting zero freq; Approx (0.09 * PWM_HPER_TICKS)  */
+#define START_SPEED_DEFAULT     (200U)                  /* Default startup speed */
+#define ACC_RAMP                (1)                     /* acceleration ramp count in internal unit */
+#define DEC_RAMP                (1)                     /* deceleration ramp count in internal unit */
+#define SPEED_FILTER_COEFF      (10)                    /* Speed filter coefficient range [1-16]    */
+
+#endif  /* ifdef AC_IM_1 */
+
+
+#endif // USERPARAMS_H
+
