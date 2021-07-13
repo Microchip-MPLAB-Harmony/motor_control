31196f10d3b48956f78f918b5b3cfc2d5c9da52a
Prachi Phadke <prachi.phadke@microchip.com>
2021-07-14 00:42:41 +0530

    [PMSM_FOC] Corrected pin for the SAMC21 MCHV3 start/stop button
    MH3-55302

--- algorithms/pmsm_foc/config/mcPmsmFoc_DigitalInterface.py
+++ algorithms/pmsm_foc/config/mcPmsmFoc_DigitalInterface.py
@@ -135,7 +135,7 @@ mcPmsmFocBoardPimParamDict =  {'MCLV2' :  {
                                                         'FAULT_LED'            : 'PB27',
                                                        },
                                             'SAMC21': {
-                                                        'START_STOP_SWITCH'    : 'PB16',
+                                                        'START_STOP_SWITCH'    : 'PA11',
                                                         'DIRECTION_SWITCH'     : 'PB31',
                                                         'DIRECTION_LED'        : 'PB12',
                                                         'FAULT_LED'            : 'PB13',
