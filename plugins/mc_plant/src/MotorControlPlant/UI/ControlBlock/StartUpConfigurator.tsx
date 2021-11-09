4754133774f63733e8237b4898135d520095de99
Dinesh Vedium <Dinesh.Vedium@microchip.com>
2021-11-09 10:48:29 +0530

    IntialField and FlyingStart tabs merge in mc plant MH3-60021

--- plugins/mc_plant/src/MotorControlPlant/UI/ControlBlock/StartUpConfigurator.tsx
+++ plugins/mc_plant/src/MotorControlPlant/UI/ControlBlock/StartUpConfigurator.tsx
@@ -3,20 +3,17 @@
 import { TabView, TabPanel } from 'primereact/tabview';
 import { Divider } from 'primereact/divider';
 import './StartUpConfigurator.css';
-import QuadratureAxis from '../../../Resources/Svgs/ControlBlock/Startup_Configuration-Initial_Field_Detection-Quadrature_Axis_Forced.svg';
-import DirectAxis from '../../../Resources/Svgs/ControlBlock/Startup_Configuration-Initial_Field_Detection-Direct_Axis_Forced.svg';
-import InitialPositionAxis from '../../../Resources/Svgs/ControlBlock/Startup_Configuration-Initial_Field_Detection-Initial_Position_Detection.svg';
-import FlyingStart from '../../../Resources/Svgs/ControlBlock/Startup_Configuration-Initial_Field_Detection-Initial_Position_Detection.svg';
-import OpenLoop from '../../../Resources/Svgs/ControlBlock/Startup_Configuration-Open_Loop_Startup.svg';
+import StartupForcedAlignDAxis from '../../../Resources/Svgs/ControlBlock/StartupConfigurator-ForcedAlign-D-Axis.svg';
+import StartupForcedAlignQAxis from '../../../Resources/Svgs/ControlBlock/StartupConfigurator-ForcedAlign-Q-Axis.svg';
+import FlyingStart from '../../../Resources/Svgs/ControlBlock/StartupConfigurator-FlyingStart.svg';
 
 import { GetSymbolArray, GetySymbolValue, mc_component_id } from '../../../MotorControlPlant/Common/SymbolAccess';
 import { GetComponent } from '../../../MotorControlPlant/Common/UIComponent';
 import useForceUpdate from 'use-force-update';
 import PropertyWindowWithImage from '../../../MotorControlPlant/Common/AddMultipleFields';
 import { getIndex } from '../../../MotorControlPlant/Common/Utils';
-import { LoadSVGasImage } from '../../Common/NodeUtils';
-// import { GetResetToDefaultStatus, callClearSymbols, GetResetStatus, ResetSymbols, UpdateResetValues } from '../CustomPopUp/CustomPopUp';
-import { useState } from 'react';
+import { LoadImage } from '../../Common/NodeUtils';
+import useWindowDimensions from '../../Common/Dimenstion';
 
 const StartUpConfigurator = (props: { showToast: (arg0: any) => void; }) => {
 
@@ -27,27 +24,26 @@ const StartUpConfigurator = (props: { showToast: (arg0: any) => void; }) => {
 
     let end_speed = "MCPMSMFOC_OL_END_SPEED";
     let startup_ramp_time = "MCPMSMFOC_OL_RAMP_TIME";
-    let startup_time = "";
 
-    let InitialFieldAllSymbolsArray = [rotor_rotation_symbolID, alignment_current, alignment_time];
     let FlyingStartAllSymbolsArray = ["MCPMSMFOC_FLYING_START",
         "MCPMSMFOC_FLYING_START_ID_KP", "MCPMSMFOC_FLYING_START_ID_KI", "MCPMSMFOC_FLYING_START_ID_KC", "MCPMSMFOC_FLYING_START_IQ_KP",
         "MCPMSMFOC_FLYING_START_IQ_KI", "MCPMSMFOC_FLYING_START_IQ_KC", "MCPMSMFOC_FLYING_START_TIME", "MCPMSMFOC_FLYING_START_SPEED",
         "MCPMSMFOC_FLYING_START_STARTUP_CURRENT", "MCPMSMFOC_FLYING_START_NULL_BRAKING_TIME", "MCPMSMFOC_FLYING_START_REGENERATIVE_BRAKING_ENABLE",
         "MCPMSMFOC_FLYING_START_REGENERATIVE_BRAKING_CURRENT", "MCPMSMFOC_FLYING_START_REGENERATIVE_BRAKING_RAMP_TIME"];
-    let OpenLoopStartUpAllSymbolsArray = [end_speed, startup_ramp_time];
 
     let selectedRotationAxis = GetySymbolValue(mc_component_id, rotor_rotation_symbolID);
 
     const update = useForceUpdate();
 
 
+    const { height, width } = useWindowDimensions();
+
     function UpdateSymbolValue(event: { value: String, name: String }) {
         (window as any).javaConnector.updateSymbolData(mc_component_id, event.name, event.value);
         update();
     }
 
-    function RotorPositonChanged(event: { value: any }) {
+    function RotorPositionChanged(event: { value: any }) {
         (window as any).javaConnector.updateSymbolData(mc_component_id, rotor_rotation_symbolID, event.value);
         selectedRotationAxis = GetySymbolValue(mc_component_id, rotor_rotation_symbolID);
         update();
@@ -58,7 +54,7 @@ const StartUpConfigurator = (props: { showToast: (arg0: any) => void; }) => {
             <div>
                 {/* <span >  Windmilling  </span>  <br></br>
                 <span > -- Parameters symbol not af  </span>  <br></br><br></br> */}
-                <span style={{ fontWeight: 'bold' }}>  Rotor Position alignment  </span>  <br></br><br></br>
+                <span style={{ fontWeight: 'bold' }}>  Field Alignment/Detection  </span>  <br></br><br></br>
                 <GetComponent componentId={mc_component_id} symbolId={alignment_current} onChange={UpdateSymbolValue} />
                 <br></br>
                 <GetComponent componentId={mc_component_id} symbolId={alignment_time} onChange={UpdateSymbolValue} />
@@ -67,54 +63,34 @@ const StartUpConfigurator = (props: { showToast: (arg0: any) => void; }) => {
         </div>);
     }
 
-    function GetIntialPositonComponents() {
-        return (<div className="p-fluid">
-            <div>
-                {/* <span >  Windmilling  </span>  <br></br>
-                <span > -- Parameters symbol not updated  </span>  <br></br><br></br>
-                <span >  Rotor Position alignment  </span>  <br></br>
-                <span > -- Parameters symbol not updated  </span>  <br></br><br></br> */}
-            </div>
-        </div>);
-    }
-
-    // function callDialogCommonInitilizeCode(SymbolArray : any){
-    //     // UpdateResetValues(SymbolArray);
-    //     alert("How")
-    //     if(GetResetToDefaultStatus()){
-    //         callClearSymbols(props.showToast, SymbolArray);
-    //         update();
-    //         alert("D")
-    //     }
-    //     // else if(GetResetStatus()){
-    //     //     ResetSymbols(props.showToast, SymbolArray);
-    //     //     update();
-    //     // }
-    // }
-
     return (
         <div className="tabbed-pane">
             <div className="card">
                 <TabView >
-                    <TabPanel header="Initial Field Detection" >
+                    <TabPanel header="Start-up" >
                         {/* {callDialogCommonInitilizeCode(InitialFieldAllSymbolsArray)} */}
                         <div className="card">
                             <br></br>
                             <div className="p-d-flex" >
                                 <div className="p-d-flex p-flex-column">
-                                    <div className="p-fluid">
-                                        <GetComponent componentId={mc_component_id} symbolId={rotor_rotation_symbolID} onChange={RotorPositonChanged} />
-                                    </div>
-                                    <label /> <br></br>
-                                    {getIndex(selectedRotationAxis, rotor_rotation_SymbolArray) === 0 && LoadSVGasImage(QuadratureAxis)}
-                                    {getIndex(selectedRotationAxis, rotor_rotation_SymbolArray) === 1 && LoadSVGasImage(DirectAxis)}
-                                    {getIndex(selectedRotationAxis, rotor_rotation_SymbolArray) === 2 && LoadSVGasImage(InitialPositionAxis)}
+                                   
+                                    
+                                    {getIndex(selectedRotationAxis, rotor_rotation_SymbolArray) === 0 && LoadImage(StartupForcedAlignQAxis)}
+                                    {getIndex(selectedRotationAxis, rotor_rotation_SymbolArray) === 1 && LoadImage(StartupForcedAlignDAxis)}
                                 </div>
                                 <Divider layout="vertical" />
 
-                                {getIndex(selectedRotationAxis, rotor_rotation_SymbolArray) === 0 && GetDirectAndQuadrature()}
-                                {getIndex(selectedRotationAxis, rotor_rotation_SymbolArray) === 1 && GetDirectAndQuadrature()}
-                                {getIndex(selectedRotationAxis, rotor_rotation_SymbolArray) === 2 && GetIntialPositonComponents()}
+                                
+                                <div className="p-fluid">
+                                <GetComponent componentId={mc_component_id} symbolId={rotor_rotation_symbolID} onChange={RotorPositionChanged} />
+                                {GetDirectAndQuadrature()}
+                                    <div><span style={{ fontWeight: 'bold' }}>  Open Loop Start-up  </span>  <br></br><br></br></div>
+                                    <div className="p-fluid">
+                                  
+                                        <div><GetComponent componentId={mc_component_id} symbolId={end_speed} onChange={UpdateSymbolValue} /><br></br></div>
+                                        <div> <GetComponent componentId={mc_component_id} symbolId={startup_ramp_time} onChange={UpdateSymbolValue} /><br></br></div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </TabPanel>
@@ -122,28 +98,15 @@ const StartUpConfigurator = (props: { showToast: (arg0: any) => void; }) => {
                         {/* {callDialogCommonInitilizeCode(FlyingStartAllSymbolsArray)} */}
                         <div className="card">
                             <div className="p-d-flex">
-                                {LoadSVGasImage(FlyingStart)}
+                            <div>
+                                        width: {width} ~ height: {height}
+                                    </div>
+                                {LoadImage(FlyingStart)}
                                 <Divider layout="vertical" />
                                 <div className="p-fluid">
                                     <div><span style={{ fontWeight: 'bold' }} >  Flying Start  </span>  <br></br><br></br></div>
                                     <div className="p-fluid">
                                         <PropertyWindowWithImage componentId={mc_component_id} parentUpdate={update} symbolsArray={FlyingStartAllSymbolsArray} />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </TabPanel>
-                    <TabPanel header="Open Loop Start-up ">
-                        {/* {callDialogCommonInitilizeCode(OpenLoopStartUpAllSymbolsArray)}    */}
-                        <div className="card">
-                            <div className="p-d-flex">
-                                {LoadSVGasImage(OpenLoop)}
-                                <Divider layout="vertical" />
-                                <div className="p-fluid">
-                                    <div><span style={{ fontWeight: 'bold' }}>  Open Loop Start-up  </span>  <br></br><br></br></div>
-                                    <div className="p-fluid">
-                                        <div><GetComponent componentId={mc_component_id} symbolId={end_speed} onChange={UpdateSymbolValue} /><br></br></div>
-                                        <div> <GetComponent componentId={mc_component_id} symbolId={startup_ramp_time} onChange={UpdateSymbolValue} /><br></br></div>
                                     </div>
                                 </div>
                             </div>
