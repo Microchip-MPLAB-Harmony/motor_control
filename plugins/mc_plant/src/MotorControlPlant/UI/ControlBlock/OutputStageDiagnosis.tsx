4a7a5eec2a2745e261ce5192b957eafce9933b15
Dinesh Vedium <Dinesh.Vedium@microchip.com>
2021-11-29 03:38:34 +0530

    Added new symbols to output stage popup MH3-60544

--- plugins/mc_plant/src/MotorControlPlant/UI/ControlBlock/OutputStageDiagnosis.tsx
+++ plugins/mc_plant/src/MotorControlPlant/UI/ControlBlock/OutputStageDiagnosis.tsx
@@ -2,16 +2,18 @@
 
 import { Divider } from 'primereact/divider';
 import icon from '../../../Resources/Svgs/ControlBlock/Output_Stage_and_Diagnosis.svg';
+import AddMultipleFields from '../../Common/AddMultipleFields';
+import { mc_component_id } from '../../Common/SymbolAccess';
 import { DialogCommonInitilizeCode } from '../CustomPopUp/CustomPopUp';
 const OutputStageDiagnosis = (props :{ parentUpdate: () => void;  showToast : (arg0: any) => void ;} ) => {
 
-    let SymbolsArray = [""];
+    let SymbolsArray = ["MCPMSMFOC_CONTROL_TO_PWM_RATIO","MCPMSMFOC_MAXIMUM_RMS_VOLTAGE"];
         
     return (
         <div>
-            {/* {DialogCommonInitilizeCode(props.showToast, SymbolsArray)} */}
+            {DialogCommonInitilizeCode(props.showToast, SymbolsArray)}
         <div className="p-d-flex" >
-            {/* <Parameters componentId = {mc_component_id} parentUpdate = {props.parentUpdate} symbolsArray = {SymbolsArray} /> */}
+            <AddMultipleFields componentId = {mc_component_id} parentUpdate = {props.parentUpdate} symbolsArray = {SymbolsArray} />
             <Divider layout="vertical" />
             <img src={icon} alt='icon' />
         </div>
